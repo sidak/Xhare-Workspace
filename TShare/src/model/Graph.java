f32df07be18e99526ac68faa13e10f3a7f11bf95
sidak <spssachdeva21695@gmail.com>
2015-12-07 17:19:24 +0530

    Add constructor and renaming as lng

--- TShare/src/model/Graph.java
+++ TShare/src/model/Graph.java
@@ -7,13 +7,16 @@ import java.util.Set;
 
 public class Graph{
 	// it is a directed graph
-	private int numVertices;
-	private Map<Point, Set<Edge>> adjList = new HashMap<Point, Set<Edge>>();
-	private Set<Point> vertices = new HashSet<Point>();
+	private Map<Point, Set<Edge>> adjList;
+	private Set<Point> vertices;
 	
-	public void addEdge(double la1, double lo1, double la2, double lo2, double dist, double sl){
-		Point u = new Point(la1, lo1);
-		Point v = new Point(la2, lo2);
+	public Graph(){
+		this.adjList = new HashMap<Point, Set<Edge>>();
+		this.vertices = new HashSet<Point>();
+	}
+	public void addEdge(double lat1, double lng1, double lat2, double lng2, double dist, double sl){
+		Point u = new Point(lat1, lng1);
+		Point v = new Point(lat2, lng2);
 		Edge e = new Edge(v, dist, sl);
 		
 		vertices.add(u);
@@ -24,6 +27,26 @@ public class Graph{
 		
 		adjList.get(u).add(e);
 		
+	}
+	
+	// use A* to calculate distance with heuristic as euclidean distance
+	public double calcEdgeDist(Point src, Point dest){
+		return aStarDist(src, dest);
+	}
+	
+	private double aStarDist(Point src, Point dest) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public int getNumVertices() {
+		return vertices.size();
+	}
+
+	public Map<Point, Set<Edge>> getAdjList() {
+		return adjList;
+	}
+	public Set<Point> getVertices() {
+		return vertices;
 	}
 	
 	
