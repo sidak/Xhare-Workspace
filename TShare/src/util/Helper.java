4060e0f0effdc555aa552a6bfb68bdf080c4a5c6
sidak <spssachdeva21695@gmail.com>
2015-12-08 12:00:27 +0530

    Add alternate method to calc distance with parameters as Point obj

--- TShare/src/util/Helper.java
+++ TShare/src/util/Helper.java
@@ -1,5 +1,7 @@
 package util;
 
+import model.Point;
+
 public class Helper {
 	
 	private static double EARTHRADIUS=3958.75;
@@ -10,6 +12,23 @@ public class Helper {
 	 */
 	public static double distBetween(double lat1, double lng1, double lat2,
 			double lng2) {
+		double dLat = Math.toRadians(lat2 - lat1);
+		double dLng = Math.toRadians(lng2 - lng1);
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+				+ Math.cos(Math.toRadians(lat1))
+				* Math.cos(Math.toRadians(lat2)) * Math.sin(dLng / 2)
+				* Math.sin(dLng / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double dist = (EARTHRADIUS*MILETOKM*1.0E3) * c;
+		return dist;
+	}
+	
+	public static double distBetween(Point src, Point dest) {
+		double lat1, lng1, lat2, lng2;
+		lat1 = src.getLat();
+		lng1 = src.getLng();
+		lat2 = dest.getLat();
+		lng2 = dest.getLng();
 		double dLat = Math.toRadians(lat2 - lat1);
 		double dLng = Math.toRadians(lng2 - lng1);
 		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
