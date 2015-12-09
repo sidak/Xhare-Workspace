9e75995befe1374f0ff30d903f995e40b93fd7b5
sidak <spssachdeva21695@gmail.com>
2015-12-09 17:19:56 +0530

    Add hashCode and equals to compare by constituent attributes instead of object

--- TShare/src/model/Point.java
+++ TShare/src/model/Point.java
@@ -33,4 +33,35 @@ public class Point {
 		
 		return false;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(lat);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(lng);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	
+	// TODO: This currently compares doubles with 1E-15 of precision. 
+	// Check if there is a need for handling them with lesser precision
+	// so as to approximate nearby vertices 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (Double.doubleToLongBits(lat) != Double.doubleToLongBits(other.lat))
+			return false;
+		if (Double.doubleToLongBits(lng) != Double.doubleToLongBits(other.lng))
+			return false;
+		return true;
+	}
 }
