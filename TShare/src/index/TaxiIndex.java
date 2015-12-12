2836f054bf1a270b232cbedaf768d035c5968d3d
sidak <spssachdeva21695@gmail.com>
2015-12-12 23:07:54 +0530

    Add TaxiIndex class

---
+++ TShare/src/index/TaxiIndex.java
@@ -0,0 +1,21 @@
+package index;
+
+import java.util.Date;
+
+public class TaxiIndex {
+	private int taxiId;
+	private Date timestamp;
+	
+	public int getTaxiId() {
+		return taxiId;
+	}
+	public void setTaxiId(int taxiId) {
+		this.taxiId = taxiId;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+}
