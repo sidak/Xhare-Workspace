5de951422fd44f9ded2cdb590c2e8ed203c95198
sidak <spssachdeva21695@gmail.com>
2015-12-16 10:52:44 +0530

    Remove size attribute from Schedule class

--- TShare/src/model/Schedule.java
+++ TShare/src/model/Schedule.java
@@ -3,14 +3,12 @@ package model;
 import java.util.List;
 
 public class Schedule {
-	private int size;
 	private List<Point> scheduleLocations;
 	private List<Long> scheduleTimes;
 	
 	public Schedule(List<Point> scheduleLocations, List<Long> scheduleTimes){
 		this.scheduleLocations = scheduleLocations;
 		this.scheduleTimes = scheduleTimes;
-		this.size = scheduleLocations.size();
 	}
 	public List<Point> getScheduleLocations() {
 		return scheduleLocations;
@@ -23,12 +21,6 @@ public class Schedule {
 	}
 	public void setScheduleTimes(List<Long> scheduleTimes) {
 		this.scheduleTimes = scheduleTimes;
-	}
-	public int getSize() {
-		return size;
-	}
-	public void setSize(int size) {
-		this.size = size;
 	}
 	
 }
